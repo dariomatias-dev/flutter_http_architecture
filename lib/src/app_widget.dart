7b7e17467201b8c546449842f96eb7be84c944d5
dariomatias-dev <matiasdario75@gmail.com>
2026-03-16 11:43:33 -0300

    feat(lib/src/app_widget.dart): defined routing scheme
    - Routing scheme defined for named route navigation.

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_http_architecture/src/screens/home/home_screen.dart';
+import 'package:flutter_http_architecture/src/screens/login/login_screen.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginScreen(),
+        '/home': (context) => HomeScreen(),
+      },
+    );
   }
 }
