0ce56a37c97526170d921000e0990a16173a038b
dariomatias-dev <matiasdario75@gmail.com>
2026-03-21 18:49:38 -0300

    refactor: simplified project
    - Removed unnecessary resources.

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:provider/single_child_widget.dart';
 
 import 'package:flutter_http_architecture/src/app_widget.dart';
 
-import 'package:flutter_http_architecture/src/core/constants/api_urls.dart';
-import 'package:flutter_http_architecture/src/core/http/dio_http_client.dart';
-import 'package:flutter_http_architecture/src/core/http/network_config.dart';
-
-import 'package:flutter_http_architecture/src/data/repositories/auth_repository.dart';
-import 'package:flutter_http_architecture/src/data/repositories/user_repository.dart';
-
 void main() {
-  runApp(
-    MultiProvider(
-      providers: <SingleChildWidget>[
-        Provider(
-          create: (_) {
-            return AuthRepository(
-              httpClient: DioHttpClient(
-                config: NetworkConfig(baseUrl: ApiUrls.dummyjson),
-              ),
-            );
-          },
-        ),
-        Provider(
-          create: (_) {
-            return UserRepository(
-              httpClient: DioHttpClient(
-                config: NetworkConfig(baseUrl: ApiUrls.dummyjson),
-              ),
-            );
-          },
-        ),
-      ],
-      child: const AppWidget(),
-    ),
-  );
+  runApp(const AppWidget());
 }
--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_http_architecture/src/screens/home/home_screen.dart';
-import 'package:flutter_http_architecture/src/screens/login/login_screen.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
@@ -11,10 +10,7 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
-      routes: {
-        '/': (context) => LoginScreen(),
-        '/home': (context) => HomeScreen(),
-      },
+      routes: {'/': (context) => HomeScreen()},
     );
   }
 }
