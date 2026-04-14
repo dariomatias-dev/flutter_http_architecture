6d6af2948cde8d4862e9a4492042591e921baea5
dariomatias-dev <matiasdario75@gmail.com>
2026-04-14 09:36:48 -0300

    feat(lib/src/core/http/models/api_response.dart): added new property
    - Added request context property.

--- lib/src/core/http/models/api_response.dart
+++ lib/src/core/http/models/api_response.dart
@@ -1,18 +1,20 @@
 import 'package:flutter_http_architecture/src/core/http/errors/http_error.dart';
+import 'package:flutter_http_architecture/src/core/http/executor/request_context.dart';
 
 class ApiResponse<T> {
   final T? data;
   final int? statusCode;
   final Map<String, dynamic>? headers;
   final HttpError? error;
+  final RequestContext? context;
 
   const ApiResponse({
-    required this.data,
+    this.data,
     this.statusCode,
     this.headers,
     this.error,
+    this.context,
   });
 
-  bool get isSuccess =>
-      error == null && statusCode != null && statusCode! < 400;
+  bool get isSuccess => error == null && (statusCode ?? 0) < 400;
 }
