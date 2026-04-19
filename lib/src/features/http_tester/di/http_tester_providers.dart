af803fd2004583137f1959d7401dfd4eb2250b4f
dariomatias-dev <matiasdario75@gmail.com>
2026-04-19 08:56:13 -0300

    feat(lib/src/features/http_tester/di/http_tester_providers.dart): add `HttpTesterNotifier`
    - Added dependency injection for `HttpTesterNotifier`.

--- lib/src/features/http_tester/di/http_tester_providers.dart
+++ lib/src/features/http_tester/di/http_tester_providers.dart
@@ -4,7 +4,9 @@ import 'package:flutter_http_architecture/src/core/di/network_providers.dart';
 
 import 'package:flutter_http_architecture/src/features/http_tester/data/repositories/http_tester_repository_impl.dart';
 import 'package:flutter_http_architecture/src/features/http_tester/domain/repositories/http_tester_repository.dart';
+import 'package:flutter_http_architecture/src/features/http_tester/presentation/providers/http_tester_notifier.dart';
 import 'package:flutter_http_architecture/src/features/http_tester/presentation/viewmodels/http_tester_view_model.dart';
+import 'package:flutter_http_architecture/src/features/http_tester/presentation/viewmodels/http_tester_view_state.dart';
 
 final httpTesterRepositoryProvider = Provider<HttpTesterRepository>((ref) {
   final client = ref.watch(httpBinClientProvider);
@@ -17,3 +19,8 @@ final httpTesterViewModelProvider = Provider<HttpTesterViewModel>((ref) {
 
   return HttpTesterViewModel(repository);
 });
+
+final httpTesterNotifierProvider =
+    AsyncNotifierProvider.autoDispose<HttpTesterNotifier, HttpTesterViewState>(
+      HttpTesterNotifier.new,
+    );
