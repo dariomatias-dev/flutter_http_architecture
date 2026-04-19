bc5d701fa52c6895f8cdffdaffbee2bf71f9e93d
dariomatias-dev <matiasdario75@gmail.com>
2026-04-19 08:54:13 -0300

    feat: install and configure Riverpod
    - Installed Riverpod package and configured it in the main file for global usage.

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:provider/single_child_widget.dart';
-
 import 'package:flutter_http_architecture/src/app_widget.dart';
-
-import 'package:flutter_http_architecture/src/core/constants/api_urls.dart';
-import 'package:flutter_http_architecture/src/core/http/client/dio_http_client.dart';
-import 'package:flutter_http_architecture/src/core/http/config/network_config.dart';
-
-import 'package:flutter_http_architecture/src/data/repositories/http_bin_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(
-    MultiProvider(
-      providers: <SingleChildWidget>[
-        Provider(
-          create: (_) {
-            return HttpBinRepository(
-              httpClient: DioHttpClient(
-                config: NetworkConfig(baseUrl: ApiUrls.httpbin),
-              ),
-            );
-          },
-        ),
-      ],
-      child: AppWidget(),
-    ),
-  );
+  runApp(ProviderScope(overrides: [], child: AppWidget()));
 }
