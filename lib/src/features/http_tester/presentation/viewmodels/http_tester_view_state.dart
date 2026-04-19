68634b1962ce25d2168d6755760c465c310611bf
dariomatias-dev <matiasdario75@gmail.com>
2026-04-19 08:43:51 -0300

    feat(lib/src/features/http_tester): create HTTP tester view state
    - Developed a class to represent the HTTP tester feature view state.

---
+++ lib/src/features/http_tester/presentation/viewmodels/http_tester_view_state.dart
@@ -0,0 +1,43 @@
+class HttpTesterViewState {
+  final String method;
+  final int statusCode;
+  final int maxRetries;
+  final String requestBody;
+  final String result;
+  final String headers;
+  final String duration;
+  final int actualRetries;
+
+  HttpTesterViewState({
+    this.method = 'GET',
+    this.statusCode = 200,
+    this.maxRetries = 0,
+    this.requestBody = '{}',
+    this.result = '',
+    this.headers = '',
+    this.duration = '',
+    this.actualRetries = 0,
+  });
+
+  HttpTesterViewState copyWith({
+    String? method,
+    int? statusCode,
+    int? maxRetries,
+    String? requestBody,
+    String? result,
+    String? headers,
+    String? duration,
+    int? actualRetries,
+  }) {
+    return HttpTesterViewState(
+      method: method ?? this.method,
+      statusCode: statusCode ?? this.statusCode,
+      maxRetries: maxRetries ?? this.maxRetries,
+      requestBody: requestBody ?? this.requestBody,
+      result: result ?? this.result,
+      headers: headers ?? this.headers,
+      duration: duration ?? this.duration,
+      actualRetries: actualRetries ?? this.actualRetries,
+    );
+  }
+}
