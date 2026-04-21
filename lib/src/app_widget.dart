cb19f57031d4f43f3542380496fcd7ffe4d3eecf
dariomatias-dev <matiasdario75@gmail.com>
2026-04-21 20:48:42 -0300

    feat(lib/src/app_widget.dart): implement theme notifier
    - Implemented the theme notifier to manage the app theme.

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:flutter_http_architecture/src/core/di/theme_notifier_provider.dart';
+import 'package:flutter_http_architecture/src/core/theme/app_theme.dart';
 
 import 'package:flutter_http_architecture/src/features/http_tester/presentation/screens/http_tester/http_tester_screen.dart';
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends ConsumerWidget {
   const AppWidget({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeNotifierProvider);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {'/': (context) => HttpTesterScreen()},
+      themeMode: themeMode,
+      theme: AppTheme.light,
+      darkTheme: AppTheme.dark,
+      home: const HttpTesterScreen(),
     );
   }
 }
